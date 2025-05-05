935d659b6230ba8c703ce0710377b15d0a4f2677
Aaryan Krishna <aaryan.krishna2024@nst.rishihood.edu.in>
2025-05-05 15:56:11 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Services from './Components/Services/Services'
 import Booking from './Components/Booking/Booking'
-import Track from './Components/Track/Track'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
-    <div>
+    <div className='container'>
       <Navbar/>
       <Hero/>
       <Services/>
       <Booking/>
-      <Track/>
       <Contact/>
       <Footer/>
     </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo-wb.png'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
+  return (   
+    <div className='navbar'>
+      <img src={logo} alt="" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>My Bookings</li>
+        <li>Contact</li>
+      </ul>
+      <div className="auth-buttons">
+          <button className="login-btn">Login</button>
+          <button className="signup-btn">Sign Up</button>
+      </div>
+      <hr/>
+    </div>
   )
 }
 
