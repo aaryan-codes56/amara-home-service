f3954f50413d0e95823ee238f3159429ea7f713a
Aaryan Krishna <aaryan.krishna2024@nst.rishihood.edu.in>
2025-05-23 13:40:57 +0530

    new

---
+++ src/routes/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Hero from '../Components/Hero/Hero';
+import Explore from '../Components/Explore/Explore';
+import Banner from '../Components/Banner/Banner';
+import Most from '../Components/Most/Most';
+import FemaleSalon from '../Components/FemaleSalon/FemaleSalon';
+import MaleSalon from '../Components/MaleSalon/MaleSalon';
+import Appliance from '../Components/Appliance/ApplianceRepair';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Explore />
+      <Banner />
+      <Most />
+      <FemaleSalon />
+      <Appliance />
+      <MaleSalon />
+
+    </>
+  );
+};
+
+export default Home;
